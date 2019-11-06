27ec137bf1d3f60b17d1b9ddb4aaec3c00c9b117
Abdulhadi Abu-Shamleh <abushamleh92@gmail.com>
2019-11-06 18:27:46 +0200

    Add user_ip and user_agenet to the log

--- sitech_audit_log/signals.py
+++ sitech_audit_log/signals.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from sitech_audit_log import AuditLogManager, AuditLog, AuditLogMixin
+from sitech_middlewares.request import get_current_request, get_current_user
+from sitech_audit_log.utils import get_user_agent, get_user_ip
 
 
 def _post_save_handler(sender, instance, created, **kwargs):
@@ -8,7 +10,13 @@ def _post_save_handler(sender, instance, created, **kwargs):
         if operation in instance.log_operation:
             values = _get_dirty_values(instance, operation)
             if values:
-                AuditLog().set_auditable(instance).set_operation(operation).set_values(values).save()
+                request = get_current_request()
+                user = get_current_user()
+                user_ip = get_user_ip(request)
+                user_agent = get_user_agent(request)
+
+                AuditLog().set_auditable(instance).set_operation(operation).set_values(values).set_creator(
+                    user).set_creator_agent(user_agent).set_creator_ip(user_ip).save()
 
 
 def _post_delete_handler(sender, instance, **kwargs):
@@ -17,7 +25,13 @@ def _post_delete_handler(sender, instance, **kwargs):
         if operation in instance.log_operation:
             values = _get_dirty_values(instance, operation)
             if values:
-                AuditLog().set_auditable(instance).set_operation(operation).set_values(values).save()
+                request = get_current_request()
+                user = get_current_user()
+                user_ip = get_user_ip(request)
+                user_agent = get_user_agent(request)
+
+                AuditLog().set_auditable(instance).set_operation(operation).set_values(values).set_creator(
+                    user).set_creator_agent(user_agent).set_creator_ip(user_ip).save()
 
 def _get_dirty_values(instance, operation):
     values = []
---
+++ sitech_audit_log/utils.py
@@ -0,0 +1,13 @@
+def get_user_ip(request):
+    if request:
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+           return x_forwarded_for.split(',')[0]
+        else:
+            return request.META.get('REMOTE_ADDR')
+    return None
+
+def get_user_agent(request):
+    if request:
+        return request.META['HTTP_USER_AGENT']
+    return None
