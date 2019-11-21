8e7eb13179271aee74d3314018605b124161bb94
Norbert Cyran <norbert@ulam.io>
2019-11-21 15:56:51 +0100

    Allow no user agent

--- sitech_audit_log/utils.py
+++ sitech_audit_log/utils.py
@@ -2,7 +2,7 @@ def get_user_ip(request):
     if request:
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
-           return x_forwarded_for.split(',')[0]
+            return x_forwarded_for.split(',')[0]
         else:
             return request.META.get('REMOTE_ADDR')
     return None
@@ -10,5 +10,5 @@ def get_user_ip(request):
 
 def get_user_agent(request):
     if request:
-        return request.META['HTTP_USER_AGENT']
+        return request.META.get('HTTP_USER_AGENT')
     return None
